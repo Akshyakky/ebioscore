77d2ecf8120630bef3573b43eaefe03713366e23
AkshyAkky <akshay@biosoftltd.com>
2024-08-14 10:34:52 +0530

    Login Page Design Improved

--- src/pages/commonPages/LoginPage/LoginPage.tsx
+++ src/pages/commonPages/LoginPage/LoginPage.tsx
@@ -8,6 +8,10 @@ import {
   Alert,
   CircularProgress,
   Link,
+  Typography,
+  Grid,
+  useTheme,
+  useMediaQuery
 } from "@mui/material";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
 import logo from "../../../assets/images/eBios.png";
@@ -23,6 +27,7 @@ import FloatingLabelTextBox from "../../../components/TextBox/FloatingLabelTextB
 import { notifySuccess } from "../../../utils/Common/toastManager";
 import { Company } from "../../../types/Common/Company.type";
 import "../../../../src/assets/styles/Common.css";
+import backgroundImage from "/src/assets/images/LoginCoverImage.jpg";
 
 const LoginPage: React.FC = () => {
   const [UserName, setUsername] = useState("");
@@ -161,7 +166,7 @@ const LoginPage: React.FC = () => {
         if (error.response) {
           setErrorMessage(
             error.response.data.errorMessage ||
-              "An error occurred during login."
+            "An error occurred during login."
           );
         } else {
           setErrorMessage("No response received from the server.");
@@ -188,109 +193,153 @@ const LoginPage: React.FC = () => {
       setErrorMessage("Please select a company");
     }
   };
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#f0f0f0",
-      }}
-    >
-      <Card sx={{ minWidth: 350 }}>
-        <CardContent>
-          <Box textAlign="center" mb={4} mt={2}>
-            <img src={logo} alt="Company Logo" style={{ maxWidth: "150px" }} />
-          </Box>
+    <Container maxWidth={false} disableGutters sx={{ height: '100vh', display: 'flex' }}>
+      <Grid container sx={{ height: '100%' }}>
+        {/* Background Image and Shapes */}
+        <Grid item xs={12} md={matches ? 12 : 8.4} sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          clipPath: matches ? 'none' : 'polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)',
+          position: "relative",
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(255, 255, 255, 0.20)',
+            clipPath: matches ? 'none' : 'inherit',
+            background: "linear-gradient(135deg, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0) 100%)"
+          },
+          '&::after': {
+            content: '""',
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            width: '20%',
+            height: '40%',
+            backgroundColor: 'rgba(255, 255, 255, 0.20)',
+            borderRadius: '50%',
+          },
+        }}>
+        </Grid>
 
-          {amcExpiryMessage && (
-            <Alert severity="warning">{amcExpiryMessage}</Alert>
-          )}
-          {licenseExpiryMessage && (
-            <Alert severity={licenseDaysRemaining <= 0 ? "error" : "warning"}>
-              {licenseExpiryMessage}
-            </Alert>
-          )}
+        {/* Login Form */}
+        <Grid item xs={12} md={matches ? 12 : 3.6} sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+        }}>
+          <Card sx={{
+            minWidth: 350,
+            maxWidth: 400,
+            boxShadow: 6,
+            borderRadius: 3,
+            backgroundColor: 'rgba(255, 255, 255, 0.9)',
+            padding: '2rem',
+          }}>
+            <CardContent>
+              <Box textAlign="center" mb={4} mt={2}>
+                <img src={logo} alt="Company Logo" style={{ maxWidth: "150px" }} />
+                <Typography variant="h5" component="h1" sx={{ mt: 2, mb: 2, fontWeight: 'bold', color: '#1976d2' }}>
+                  Welcome to e-Bios
+                </Typography>
+              </Box>
 
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <DropdownSelect
-              label="Select Company"
-              name="companyID"
-              value={
-                companyID && companyCode ? `${companyID},${companyCode}` : ""
-              }
-              options={companies.map((c) => ({
-                value: c.compIDCompCode,
-                label: c.compName,
-              }))}
-              onChange={(event) => {
-                const compIDCompCode = event.target.value as string;
-                const selectedCompany = companies.find(
-                  (c) => c.compIDCompCode === compIDCompCode
-                );
-                handleSelectCompany(
-                  compIDCompCode,
-                  selectedCompany?.compName || ""
-                );
-              }}
-              size="small"
-            />
+              {amcExpiryMessage && (
+                <Alert severity="warning">{amcExpiryMessage}</Alert>
+              )}
+              {licenseExpiryMessage && (
+                <Alert severity={licenseDaysRemaining <= 0 ? "error" : "warning"}>
+                  {licenseExpiryMessage}
+                </Alert>
+              )}
 
-            <FloatingLabelTextBox
-              ControlID="username"
-              title="Username"
-              value={UserName}
-              onChange={(e) => setUsername(e.target.value)}
-              size="small"
-              isMandatory
-            />
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <DropdownSelect
+                  label="Select Company"
+                  name="companyID"
+                  value={
+                    companyID && companyCode ? `${companyID},${companyCode}` : ""
+                  }
+                  options={companies.map((c) => ({
+                    value: c.compIDCompCode,
+                    label: c.compName,
+                  }))}
+                  onChange={(event) => {
+                    const compIDCompCode = event.target.value as string;
+                    const selectedCompany = companies.find(
+                      (c) => c.compIDCompCode === compIDCompCode
+                    );
+                    handleSelectCompany(
+                      compIDCompCode,
+                      selectedCompany?.compName || ""
+                    );
+                  }}
+                  size="small"
+                />
 
-            <FloatingLabelTextBox
-              ControlID="password"
-              title="Password"
-              type="password"
-              value={Password}
-              onChange={(e) => setPassword(e.target.value)}
-              size="small"
-              isMandatory
-            />
+                <FloatingLabelTextBox
+                  ControlID="username"
+                  title="Username"
+                  value={UserName}
+                  onChange={(e) => setUsername(e.target.value)}
+                  size="small"
+                  isMandatory
+                />
 
-            <Box textAlign="right">
-              <Link href="/ForgotPasswordPage" variant="body2">
-                Forgot password?
-              </Link>
-            </Box>
+                <FloatingLabelTextBox
+                  ControlID="password"
+                  title="Password"
+                  type="password"
+                  value={Password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  size="small"
+                  isMandatory
+                />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{
-                mt: 3,
-                mb: 2,
-                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-              }}
-              disabled={isLoggingIn}
-              startIcon={
-                isLoggingIn ? <CircularProgress size={20} /> : <LockOpenIcon />
-              }
-            >
-              {isLoggingIn ? "Signing In..." : "Sign In"}
-            </Button>
+                <Box textAlign="right" sx={{ mb: 2 }}>
+                  <Link href="/ForgotPasswordPage" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Box>
 
-            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-          </Box>
-        </CardContent>
-      </Card>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+                  }}
+                  disabled={isLoggingIn}
+                  startIcon={
+                    isLoggingIn ? <CircularProgress size={20} /> : <LockOpenIcon />
+                  }
+                >
+                  {isLoggingIn ? "Signing In..." : "Sign In"}
+                </Button>
+
+                {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
