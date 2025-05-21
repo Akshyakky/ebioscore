f5ccc27cb445ac4660b318f399be144c1937508a
Mithun T <mithun@biosoftltd.com>
2025-05-21 15:50:27 +0530

    setup DiagnosisList for react-hook-form

--- src/pages/clinicalManagement/DiagnosisList/MainPage/DiagnosisListPage.tsx
+++ src/pages/clinicalManagement/DiagnosisList/MainPage/DiagnosisListPage.tsx
@@ -1,12 +1,7 @@
-// src/pages/clinicalManagement/DiagnosisList/MainPage/DiagnosisListPage.tsx
 import React from "react";
-import { IcdDetailDto } from "@/interfaces/ClinicalManagement/IcdDetailDto";
-import DiagnosisDetails from "../SubPage/DiagnosisDetails";
-import DiagnosisSearch from "../SubPage/DiagnosisSearch";
-import MedicalEntityPage from "../../Components/MedicalEntityPage/MedicalEntityPage";
 
 const DiagnosisListPage: React.FC = () => {
-  return <MedicalEntityPage<IcdDetailDto> title="Diagnosis List" DetailComponent={DiagnosisDetails} SearchComponent={DiagnosisSearch} />;
+  return <></>;
 };
 
 export default React.memo(DiagnosisListPage);
