5edfb028406276990db18135a21fdfcf76310985
Mithun T <mithun@biosoftltd.com>
2025-05-21 16:07:38 +0530

    feat: add MedicationDosageForm component and remove unused MedicationDosageDetails and MedicationDosageSearch components

---
+++ src/pages/clinicalManagement/MedicationDosage/Form/MedicationDosageForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MedicationDosageForm: React.FC = () => {
+  return <></>;
+};
+
+export default MedicationDosageForm;
--- src/pages/clinicalManagement/MedicationDosage/MainPage/MedicationDosagePage.tsx
+++ src/pages/clinicalManagement/MedicationDosage/MainPage/MedicationDosagePage.tsx
@@ -1,12 +1,7 @@
-// src/pages/clinicalManagement/MedicationDosage/MainPage/MedicationDosagePage.tsx
 import React from "react";
-import { MedicalEntityPage } from "../../Components/MedicalEntityPage/MedicalEntityPage";
-import MedicationDosageDetailsNew from "../SubPage/MedicationDosageDetails";
-import MedicationDosageSearchNew from "../SubPage/MedicationDosageSearch";
-import { MedicationDosageDto } from "@/interfaces/ClinicalManagement/MedicationDosageDto";
 
 const MedicationDosagePage: React.FC = () => {
-  return <MedicalEntityPage<MedicationDosageDto> title="Medication Dosage" DetailComponent={MedicationDosageDetailsNew} SearchComponent={MedicationDosageSearchNew} />;
+  return <></>;
 };
 
 export default MedicationDosagePage;
