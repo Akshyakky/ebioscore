c0980b358bf18f59cde9bedc7c175cbaa68c5491
Akshyakky <akshay.putta3@gmail.com>
2025-05-22 16:02:01 +0530

    refactor: update page title management and breadcrumb navigation
    - Replaced static page title mapping with dynamic retrieval from route configuration in usePageTitle hook.
    - Simplified BreadcrumbsNavigation component by removing hardcoded path mappings and utilizing route metadata for display names and categories.
    - Enhanced routeConfig to include metadata for titles and categories, improving maintainability and consistency across the application.

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -1,60 +1,14 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-
-interface PageTitleMap {
-  [key: string]: string;
-}
-
-const pageTitleMap: PageTitleMap = {
-  "/login": "Login",
-  "/dashboard": "Dashboard",
-  "/RegistrationPage": "Registration",
-  "/RoutineReportsPA": "Routine Reports [PA]",
-  "/RevisitPage": "Revisit",
-  "/ListOfReportsPage": "Routine Reports [FO]",
-  "/ContactListPage": "Contact List",
-  "/UserListPage": "User List",
-  "/ProfileListPage": "Profile List",
-  "/AdmissionPage": "Patient Admission",
-  "/AppointmentPage": "Appointment",
-  "/ReasonListPage": "Reason List",
-  "/ResourceListPage": "Resource List",
-  "/BreakListPage": "Break List",
-  "/PatientInvoiceCodePage": "Patient Invoice Code List",
-  "/DepartmentListPage": "Department List",
-  "/PaymentTypesPage": "Payments Type List",
-  "/ServiceGroupsListPage": "Service Groups List",
-  "/AlertPage": "Alert Manager",
-  "/WardCategoryPage": "Room-Bed Category List",
-  "/BedSetUpPage": "Room-Bed Set-Up",
-  "/DeptUnitListPage": "Department Unit List",
-  "/InsuranceListPage": "Insurance List",
-  "/ProductListPage": "Product List",
-  "/ProductTaxListPage": "Product Tax List",
-  "/ProductOverviewPage": "Product Overview ",
-  "/ManageBedPage": "Manage BedPage ",
-  "/DiagnosisListPage": "Diagnosis List ",
-  "/MedicationListPage": "Medication List ",
-  "/MedicationFormPage": "Medication Form ",
-  "/AppModifiedListPage": "App Modified List",
-  "/ChargeDetailsPage": "Schedule of Charge List",
-  "/DischargePage": "Patient Discharge ",
-  "/WardBedTransferPage": "Patient Ward Bed Transfer ",
-  "/MedicationFrequencyPage": "Medication Frequency",
-  "/MedicationDosagePage": "Medication Dosage",
-  "/ProcedureListPage": "Procedure List",
-  "/MedicationGenericPage": "Medication Generic",
-  "/InvestigationListPage": "Investigation List",
-  "/ComponentEntryTypePage": "Component EntryType Page ",
-  "/IndentProductPage": "Indent Product",
-};
+import routeConfig from "@/routes/routeConfig";
 
 export const usePageTitle = () => {
   const [pageTitle, setPageTitle] = useState("eBios - Healthcare Solution");
   const location = useLocation();
 
   useEffect(() => {
-    const titleSuffix = pageTitleMap[location.pathname] || "eBios Healthcare Solution";
+    const route = routeConfig.find((route) => route.path.toLowerCase() === location.pathname.toLowerCase());
+    const titleSuffix = route?.metadata?.title || "eBios Healthcare Solution";
     const title = `eBios - ${titleSuffix}`;
     document.title = title;
     setPageTitle(titleSuffix);
