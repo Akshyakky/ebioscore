dbc86611237d3125f1b835bb73b7e0f6cbcbf2bb
Mithun T <mithun@biosoftltd.com>
2025-06-02 12:28:08 +0530

    feat: add DeptUnitAllocationDto interface for department unit allocation data structure

---
+++ src/interfaces/HospitalAdministration/DeptUnitAllocationDto.ts
@@ -0,0 +1,31 @@
+export interface DeptUnitAllocationDto {
+  dUAID: number;
+  deptID: number;
+  deptName?: string;
+  dulID: number;
+  unitDesc?: string;
+  uASTIME: string;
+  uAETIME: string;
+  facultyID: number;
+  facultyName?: string;
+  roomID: number;
+  roomName?: string;
+  resourceID: number;
+  resourceName?: string;
+  occurance1YN?: string;
+  occurance2YN?: string;
+  occurance3YN?: string;
+  occurance4YN?: string;
+  occurance5YN?: string;
+  occuranceAllYN?: string;
+  sunYN?: string;
+  monYN?: string;
+  tueYN?: string;
+  wedYN?: string;
+  thuYN?: string;
+  friYN?: string;
+  satYN?: string;
+  allDaysYN?: string;
+  specialityID?: number;
+  unitHeadYN?: string;
+}
