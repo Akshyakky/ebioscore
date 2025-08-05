f5cdcfb53753d40b1713964136cc85e4f1643284
mridul_v <mridul@biosoftltd.com>
2025-08-05 17:34:18 +0530

    Discharge Summary setup

--- src/services/PatientAdministrationServices/patientAdministrationService.ts
+++ src/services/PatientAdministrationServices/patientAdministrationService.ts
@@ -1,6 +1,6 @@
 import { DischargeStatusDto } from "@/interfaces/PatientAdministration/DischargeStatusDto";
-import { IpDischargeDetailsDto } from "@/interfaces/PatientAdministration/IpDischargeDetailDto";
+import { IpDischargeDetailDto } from "@/interfaces/PatientAdministration/IpDischargeDetailDto";
 import { createEntityService } from "@/utils/Common/serviceFactory";
 
 export const dischargeStatusService = createEntityService<DischargeStatusDto>("DischargeStatus", "patientAdministrationURL");
-export const dischargeSummaryService = createEntityService<IpDischargeDetailsDto>("DischargeSummary", "patientAdministrationURL");
+export const dischargeSummaryService = createEntityService<IpDischargeDetailDto>("DischargeDetail", "patientAdministrationURL");
